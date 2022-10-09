4824fcb67983aef675d98d42e1045f86be584ccc
ivgam <ivan.garcia.maya@gmail.com>
2022-10-09 10:19:50 -0600

    feat: adding more exercises

---
+++ Intersection of Two Arrays II/solution.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+let intersect = function(nums1, nums2) {
+    let h1 = {};
+    let h2 = {};
+    let result = [];
+    
+    fillHash(nums1, h1);I
+    fillHash(nums2, h2);
+    
+    Object.keys(h1).forEach(function(k){
+        if(h2[k]){
+            let reps = Math.min(h1[k], h2[k]);
+            for(let i=0; i<reps; i++){
+                result.push(k);
+            }
+        }
+    })
+    
+    return result;
+};
+
+let fillHash = function (values, hash){
+    for(let i=0; i<values.length; i++){
+        if(hash[values[i]]){
+            hash[values[i]]++;
+        } else {
+            hash[values[i]] = 1;
+        }
+    }    
+}
